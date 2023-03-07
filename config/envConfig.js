283cdf8ad1f5a340fd61215e26d6fd80730fcbc3
teklu <teklufa@gmail.com>
2023-03-07 16:22:37 +0300

    install and setup dotenv for development

---
+++ config/envConfig.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+
+if(process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
+
+const PORT = process.env.PORT
+const NODE_ENV = process.env.NODE_ENV
+
+export default {
+  PORT,
+  NODE_ENV
+}
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import http from 'http'
 import app from './app.js'
+import envConfig from './config/envConfig.js'
 
-const PORT = process.env.PORT || 3001
+const PORT = envConfig.PORT || 3001
 
 // const server = http.createServer(app)
 const server = http.createServer()
 server.on('request', app)
 
 server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT} in ${envConfig.NODE_ENV} mode`);
 })
